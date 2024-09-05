4b7039bb7ecd2c6b1ee7b927122f0db76b5a1ea3
Zack Bentley <zpbentley99@gmail.com>
2024-09-05 14:17:17 -0400

    calc can weight by destinationC:/Users/ztben/AppData/Local/Microsoft/WindowsApps/python3.12.exe c:/Users/ztben/workspace/FedExTrips/trips.py

--- trips.py
+++ trips.py
@@ -4,18 +4,17 @@ import openpyxl
 wb = openpyxl.load_workbook('Excel-Documents\\WBManifestTable_1706103354202.xlsx')
 sheet = wb['FedEx Air Ops Workbench Report']
 
+tuple(sheet['B5':'E46'])
+
 
 # Gets ULD number from the Excel sheet
-def getCanNum():
+def getCanNum(cans):
     arrayOfCans = []
 
-    for rowOfCellObjects in sheet['B5':'B46']:
-        for cellObj in rowOfCellObjects:
-            if cellObj.value == 'None':
-                break
-            else:
-                arrayOfCans.append(cellObj.value)
+    for can in cans:
+        arrayOfCans.append(can.value)
     
+    print(arrayOfCans)
     return arrayOfCans
 
 # Cut the can number to the type of can which is always the first 3 characters
@@ -23,8 +22,7 @@ def checkUldType(arrayOfCans):
     typesOfCans = []
     
     for canNum in arrayOfCans:
-        type = canNum[:3]
-        typesOfCans.append(type)
+        typesOfCans.append(canNum[:3])
 
     return typesOfCans
 
@@ -48,25 +46,50 @@ def weightOfCan(type):
 
     return canWeight
 
-# Main function
-def calcWeight():
+def getWeight(weightOfCans):
     sumTotal = 0
-    for rowOfCellObjects in sheet['D5':'D46']:
-        for num in rowOfCellObjects:
-            if num.value:
-                weight = int(num.value)
-                sumTotal += weight
-    
-    print('With can weight, the total weight of freight is %s' % sumTotal)
 
-    cantypes = getCanNum()
-    ulds = checkUldType(cantypes)
-    for uld in ulds:
-        sumTotal = sumTotal - weightOfCan(uld)
+    for cell in weightOfCans:
+        if cell.value:
+            sumTotal += int(cell.value)
     
-    print('After complicated math here is the total freight weight without those pesky cans: %s' % sumTotal)
     return sumTotal
 
+def getDestCans(dest):
+    destCoords = []
 
+    for rowOfCellObjects in sheet['E5':'E46']:
+        for cellObj in rowOfCellObjects:
+            if cellObj.value == dest:
+                rowNum = cellObj.coordinate[1:]
 
-calcWeight()
+                destCoords.append(rowNum)
+
+    return destCoords
+
+# Main function
+def calcWeight(dest):
+    destCans = getDestCans(dest)
+
+    weightCells = [sheet[f'D{num}'] for num in destCans]
+
+    sumTotal = getWeight(weightCells)
+    print(f'With can weight, the total weight of freight is {sumTotal}')
+
+    canNumCells = [sheet[f'B{num}'] for num in destCans]
+    cantypes = getCanNum(canNumCells)
+
+    ulds = checkUldType(cantypes)
+
+    for uld in ulds:
+        sumTotal -= weightOfCan(uld)
+    
+    print(f'After complicated math here is the total freight weight without those pesky cans: {sumTotal}')
+    return sumTotal
+
+# for a specific uld destination
+dest = input("For which destination do you seek?\n('FFTA', 'CVGA', 'LUKA')>>> ")
+# dest = 'cvgrt'
+upperDest = dest.upper()
+
+calcWeight(upperDest)
